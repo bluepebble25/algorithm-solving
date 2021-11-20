cb211f54650acb6c89e13659eb44729341b1a324
bluepebble25 <lhyeon25@gmail.com>
2021-11-21 00:10:09 +0900

    수학,정수론,소수 판정,에라토스테네스의 체

--- BOJ/1978_20211031_소수 찾기.py
+++ BOJ/1978_20211031_소수 찾기.py
@@ -1,3 +1,21 @@
+def isPrime(num):
+    if num == 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for k in range(2, num): # 2부터 자기자신보다 1작은 수로 나누어떨어지면 소수 X
+            if num % k == 0:
+                return False
+        return True
+
 N = int(input())
 num_list = list(map(int, input().split()))
+count = 0
 
+for num in num_list:
+    if isPrime(num):
+        count += 1
+             
+print(count)
+        
